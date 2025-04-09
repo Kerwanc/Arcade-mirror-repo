89a90cdb33d060980e2e3948ea33d7ea53127d65
Calvier Kerwan <kerwan.calvier@epitech.eu>
2025-04-09 21:13:50 +0200

    feat(sfml): sfml now display square of entity color

--- src/graphic/Sfml.cpp
+++ src/graphic/Sfml.cpp
@@ -6,10 +6,11 @@
 */
 
 #include <iostream>
+#include <fstream>
 
 #include "Sfml.hpp"
 
-SFML::SFML() : window_(sf::VideoMode({1920, 1080}), "SFML works!")
+SFML::SFML() : window_(RESOLUTION, "SFML works!")
 {
 
 }
@@ -23,8 +24,21 @@ event_t SFML::getEvent()
 {
     event_t event;
     sf::Event sfml_event;
+    sf::Vector2i mouse_pos = sf::Mouse::getPosition(window_);
 
     while(window_.pollEvent(sfml_event)) {
+        if (sfml_event.type == sf::Event::MouseMoved) {
+            event.events.push_back(A_MOUSE_MOVE);
+            event.mPos = {(double)mouse_pos.x / RESOLUTION.height * 100,
+                (double)mouse_pos.y / RESOLUTION.width * 100};
+        }
+        if (sfml_event.type == sf::Event::MouseButtonPressed) {
+            for (const auto &it : MOUSE_EVENT_LINK) {
+                event.events.push_back(it.first);
+            }
+            event.mPos = {(double)mouse_pos.x / RESOLUTION.height * 100,
+                (double)mouse_pos.y / RESOLUTION.width * 100};
+        }
         if (sfml_event.type == sf::Event::KeyPressed) {
             for (const auto &it : KEYBOARD_EVENT_LINK) {
                 if (sfml_event.key.code == it.second) {
@@ -36,10 +50,38 @@ event_t SFML::getEvent()
     return event;
 }
 
+void SFML::createEntities(std::vector<entity_t> entities)
+{
+    std::vector<sf::Texture> textures;
+    sf::RectangleShape rectangle;
+    sf::Texture texture;
+
+    for (const auto &entity : entities) {
+        std::ifstream inputFile(entity.asset);
+        if (inputFile) {
+        } else {
+            rectangle.setSize({(float)entity.size.x* window_.getSize().x / 100, (float)entity.size.y* window_.getSize().y / 100});
+            rectangle.setFillColor(sf::Color{entity.color.r, entity.color.g, entity.color.b, entity.color.a});
+            rectangle.setPosition(entity.pos.x * window_.getSize().x / 100, entity.pos.y * window_.getSize().y / 100);
+            rectangles_.push_back(rectangle);
+        }
+    }
+}
+
 void SFML::display(data_t data)
 {
+    rectangles_.clear();
+    sprites_.clear();
+    createEntities(data.bg);
+    createEntities(data.objects);
+    createEntities(data.ui);
     window_.clear();
-    // window_.draw();
+    for (const auto &sprite : sprites_) {
+        window_.draw(sprite);
+    }
+    for (const auto &rectangle : rectangles_) {
+        window_.draw(rectangle);
+    }
     window_.display();
 }
 
--- src/graphic/Sfml.hpp
+++ src/graphic/Sfml.hpp
@@ -19,8 +19,11 @@ class SFML : public arcade::IGraphic {
         ~SFML();
         event_t getEvent(void) override;
         void display(data_t data) override;
+        void createEntities(std::vector<entity_t> entities);
     private:
         sf::RenderWindow window_;
+        std::vector<sf::Sprite> sprites_;
+        std::vector<sf::RectangleShape> rectangles_;
 };
 
 const std::map<event_e, sf::Keyboard::Key> KEYBOARD_EVENT_LINK =
@@ -30,8 +33,17 @@ const std::map<event_e, sf::Keyboard::Key> KEYBOARD_EVENT_LINK =
     {A_KEY_UP, sf::Keyboard::Up},
     {A_KEY_LEFT, sf::Keyboard::Left},
     {A_KEY_RIGHT, sf::Keyboard::Right},
-    {A_KEY_DOWN, sf::Keyboard::Down}
+    {A_KEY_DOWN, sf::Keyboard::Down},
+    {A_KEY_ENTER, sf::Keyboard::Enter}
 };
+
+const std::map<event_e, sf::Mouse::Button> MOUSE_EVENT_LINK =
+{
+    {A_MOUSE_LEFT, sf::Mouse::Left},
+    {A_MOUSE_RIGHT, sf::Mouse::Right}
+};
+
+const sf::VideoMode RESOLUTION = {1000, 1000};
 
 extern "C" arcade::IGraphic* makeGraphic();
 
