939144ec13c417345637ac537ef3ca05834f7e09
celianrag <celian.raguin@epitech.eu>
2025-04-10 02:29:33 +0200

    feat(snake): adding the snake in snake and creating its movement

--- src/games/Snake.cpp
+++ src/games/Snake.cpp
@@ -19,16 +19,78 @@ bool isAtBorder(size_t x, size_t y)
     return false;
 }
 
+static entity_t createSnakePart(char part, color_e color, vector_t pos, vector_t size)
+{
+    entity_t element;
+
+    element.asset = TILES_ASSETS.at(part);
+    element.pos = pos;
+    element.color = TILES_COLOR[color];
+    element.direction = LEFT;
+    element.character = part;
+    element.size = size;
+    return element;
+}
+
+static std::vector<entity_t> setSnake(void)
+{
+    std::vector<entity_t> snake;
+
+    for (size_t i = 0; i < SNAKE_INIT_SIZE; i++) {
+        if (i == 0) {
+            snake.push_back(createSnakePart('<', YELLOW, SNAKE_START, {1.0, 1.0}));
+        } else {
+            snake.push_back(createSnakePart('X', GREEN, {SNAKE_START.x + i, SNAKE_START.y}, {1.0, 1.0}));
+        }
+    }
+    return snake;
+}
+
 Snake::Snake()
 {
+    generateMap();
+    snake_ = setSnake();
+    for (auto snakePart : snake_)
+        data_.objects.push_back(snakePart);
+    clock_ = 0;
+    direction_ = A_KEY_LEFT;
 }
 
-void Snake::handleEvent(event_t events)
+void Snake::moveSnake(event_e directionKey)
 {
+    vector_t actualPose = snake_.front().pos;
+    vector_t direction = DIRECTIONS_RATIO.at(directionKey).ratio;
+    vector_t newPose = {actualPose.x + direction.x,
+                        actualPose.y + direction.y};
 
+    snake_.front().character = DIRECTIONS_RATIO.at(directionKey).character;
+    for (auto &snakePart : snake_) {
+        actualPose = snakePart.pos;
+        snakePart.pos = newPose;
+        newPose = actualPose;
+    }
 }
 
-static entity_t createBgElement(vector_t pos, char type)
+void Snake::handleEvent(event_t latestEvent)
+{
+    data_.objects.clear();
+
+    for (auto event : latestEvent.events) {
+        if (DIRECTIONS_RATIO.contains(event)) {
+            direction_ = event;
+        }
+    }
+    if (clock_ == 50) {
+        moveSnake(direction_);
+        clock_ = 0;
+    } else {
+        clock_ += 1;
+    }
+    for (auto snakePart : snake_)
+        data_.objects.push_back(snakePart);
+}
+
+static entity_t createBgElement(vector_t size, vector_t pos, char type)
 {
     entity_t element;
 
@@ -37,23 +99,28 @@ static entity_t createBgElement(vector_t pos, char type)
     element.color = TILES_COLOR[WHITE];
     element.direction = UP;
     element.character = type;
+    element.size = size;
     return element;
 }
 
 void Snake::generateMap(void)
 {
     char type = ' ';
+    size_t trueX = 0;
+    size_t trueY = 0;
 
-    for (size_t y = 0; y < MAP_SIZE; y++) {
+    for (size_t y = 0 ; y < MAP_SIZE; y++) {
         map_.push_back({});
-        for (size_t x = 0; x < MAP_SIZE; x++) {
+        for (size_t x = 0 ; x < MAP_SIZE; x++) {
+            trueX = x + CENTERING.x;
+            trueY = y + CENTERING.y;
             if (isAtBorder(x, y)) {
                 type = '#';
             } else {
                 type = ' ';
             }
-            map_[y].push_back({{3 * y, 3 * x}, type});
-            data_.bg.push_back(createBgElement({3.0 * y, 3.0 * x}, type));
+            map_[y].push_back({{trueY, trueX}, type});
+            data_.bg.push_back(createBgElement({1.0, 1.0}, {(double)trueY, (double)trueX}, type));
         }
     }
 }
--- src/games/Snake.hpp
+++ src/games/Snake.hpp
@@ -13,6 +13,10 @@
     #include <map>
 
     const size_t MAP_SIZE = 30;
+    const size_t SNAKE_INIT_SIZE = 3;
+    const vector_t CENTERING = {50, 10};
+    const vector_t SNAKE_START = {50, 50};
+
 
 typedef struct coord_s {
     size_t x;
@@ -31,27 +35,56 @@ class Snake : public arcade::IGame {
         ~Snake();
         void handleEvent(event_t events) override;
         data_t update(void) override;
+        void moveSnake(event_e direction);
         void generateMap(void) ;
     private:
         data_t data_;
+        size_t clock_;
+        event_e direction_;
         std::vector<std::vector<tileInfo_t>> map_;
+        std::vector<entity_t> snake_;
 };
 
 /* ASSETS */
 
 const std::map<char, std::string> TILES_ASSETS = {
     {'#', " "},
-    {' ', " "}
+    {' ', " "},
+    {'<', " "},
+    {'>', " "},
+    {'^', " "},
+    {'+', " "},
+    {'X', " "}
+};
+
+
+typedef struct ratioValues_s {
+    char character;
+    vector_t ratio;
+} ratioValues_t;
+
+const std::map<event_e, ratioValues_t> DIRECTIONS_RATIO =
+{
+    {A_KEY_DOWN, {'+', {0.0, 1}}},
+    {A_KEY_LEFT, {'<', {-1, 0.0}}},
+    {A_KEY_RIGHT, {'>', {1, 0.0}}},
+    {A_KEY_UP, {'^', {0.0, -1}}}
 };
 
 enum color_e {
     WHITE,
-    BLACK
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW
 };
 
 const color_t TILES_COLOR[] {
     {255, 255, 255, 255},
-    {0, 0, 0, 255}
+    {0, 0, 0, 255},
+    {255, 0, 0, 255},
+    {0, 255, 0, 255},
+    {255, 255, 0, 255}
 };
 
 const std::string BACKGROUND_ASSETS = "";
